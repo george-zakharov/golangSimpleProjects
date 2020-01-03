6d2c92b8ea237a816726535bfe0f7f0193662657
George Zakharov <george_zakharov@mail.ru>
2020-01-03 22:36:40 +0300

    Codestyle fix

--- simleServer.go
+++ simleServer.go
@@ -1,12 +1,16 @@
 package main
+
 import (
 	"fmt"
 	"net/http"
 )
+
 type msg string
+
 func (m msg) ServeHTTP(resp http.ResponseWriter, req *http.Request) {
 	fmt.Fprint(resp, m)
 }
+
 func main() {
 	msgHandler := msg("Hello from Web Server in Go")
 	fmt.Println("Server is listening...")
